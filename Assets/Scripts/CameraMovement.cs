27228eb19ba1010c37cfbb75101427f74078c708
Matthis Hagen <matthis.hagen@o2online.de>
2018-10-27 04:10:06 +0200

    Add Trap Assets;
    Add simple Capsule-Collider to player Character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed;
+    [SerializeField]
+    BoxCollider collider;
+    bool hin = true;
+    bool her = false;
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector3(1, 1, 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xmovement = Input.GetAxis("Horizontal");
+        float ymovement = Input.GetAxis("Vertical");
+        // Input.mousePosition 
+        // Mathf.
+        if (hin && (Input.GetKey("n") || transform.eulerAngles.x>=60))
+        {
+            hin = false;
+            her = true;
+        }
+        if (her && (Input.GetKey("b") || transform.eulerAngles.x <= -60))
+        {
+            hin = true;
+            her = false;
+        }
+        if (hin==true) {
+            //transform.position += new Vector3(0, 2 * movementSpeed * Time.deltaTime, 0);
+            transform.Rotate(Vector3.right, 30 * movementSpeed * Time.deltaTime);
+        }
+        
+        if (her == true) {
+            //transform.position += new Vector3(0, -2 * movementSpeed * Time.deltaTime, 0);
+            transform.Rotate(Vector3.right, -30 * movementSpeed * Time.deltaTime);
+        }
+            //transform.position += new Vector3(xmovement * movementSpeed * Time.deltaTime, ymovement * movementSpeed * Time.deltaTime, 0);
+            //collider.size += new Vector3(xmovement * movementSpeed * Time.deltaTime, ymovement * movementSpeed * Time.deltaTime, 0);
+
+        }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.rotation = Quaternion.identity;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Ouch!");
+    }
+}
