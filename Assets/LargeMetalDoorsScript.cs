ae8e802a85118799cf4f5e3dbb36ee492fc8dfa2
Oleksii <alexey.surghenko@gmail.com>
2018-10-27 18:31:33 +0200

    update - AlexeyScene

---
+++ Assets/LargeMetalDoorsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeMetalDoorsScript : MonoBehaviour {
+	private bool zoneDoor = false;
+	private bool doorOpened = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Door - Hebel
+		if(!doorOpened && zoneDoor && Input.GetKeyDown(KeyCode.G))
+		{
+			GetComponent<Animator>().SetTrigger("LargeDoorOpen");
+			doorOpened = true;
+		}
+	}
+	private void OnTriggerEnter(Collider Player)
+    {
+		zoneDoor = true;
+		GameObject.Find("[Key] - Press G to open the door").GetComponent<UnityEngine.UI.Text>().enabled = true;
+	}
+	private void OnTriggerExit(Collider Player)
+    {
+		zoneDoor = false;
+		GameObject.Find("[Key] - Press G to open the door").GetComponent<UnityEngine.UI.Text>().enabled = false;
+	}
+}
