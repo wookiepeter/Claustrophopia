2dabe210943728e62ef210f91bd4fc9aa8d0d33d
Oleksii <alexey.surghenko@gmail.com>
2018-10-28 18:00:20 +0100

    update - AlexeyScene

---
+++ Assets/MonsterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour {
+	public bool start;
+	public Animator monster;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+			start = true;
+			if(start) monster.SetBool("Start", true);
+		}
+	}
+	private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+			start = false;
+			if(!start) monster.SetBool("Start", false);
+		}
+	}
+}
